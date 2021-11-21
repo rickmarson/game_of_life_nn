9e8e6f4306d461b631b2895423c4ec782c571272
rickmarson <marson.riccardo@gmail.com>
2021-11-21 09:17:19 +0000

    - deleted skeleton of mazes project - imported into WSL

--- common/mazes/lib/mazes.hpp
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#include <stdint.h>
-
-[[nodiscard]] std::vector<uint8_t> gen_test_image();
--- common/mazes/test/test_mazes.cpp
+++
@@ -1,5 +0,0 @@
-#include "../lib/mazes.hpp"
-
-int main(int argc, char* argv[]) {
-    auto pixels = gen_test_image();
-}
